c3113c554e1101180128720eadca5fb6a115f260
mahmutkara01 <bilgimahmutkara@gmail.com>
2023-05-20 16:27:35 +0300

    Initial Commit

---
+++ MVVM-project/MVVM_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_projectApp.swift
+//  MVVM-project
+//
+//  Created by mahmut kara on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
